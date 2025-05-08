a2968ffdeb6a80984b8e6ef3570c5f34f850ef16
Alka Tripathi <tripathialka382@gmail.com>
2025-05-08 15:30:17 +0530

    added new tasks

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,18 +1,36 @@
 import { useState } from "react";
 function Input(){
-    let [input,setInput]=useState("");
-    let [task,setTask]=useState("Sample state");
+    let [input,setInput]=useState("");  //input task 
+    let [task,setTask]=useState([]);     //input array
 
     const handleInput=(e)=>{
         let data=e.target.value
-        setTask(data);
+        setInput(data);
+    }
+
+    const addTasks=(e)=>{
+        // e.preventDefault();
+        e.preventDefault();
+        if(input.trim()){
+            setTask([...task,input]);
+            setInput(" ");
+
+        }
+
     }
     return(
        <form>
         <h1>To-Do List</h1>
         <input placeholder="Enter your tasks" name={input} type="text" onChange={handleInput}></input>
+        <button onClick={addTasks}>Add Tasks</button>
 
         <ul>
+            {task.map((item,index)=>{
+               return(
+                <li key={index}>{item}     <button> Delete Task</button></li>
+            
+               )
+            })}
           
         </ul>
        </form>
